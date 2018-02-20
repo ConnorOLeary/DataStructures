adad96a6fc572f1304165aa791a053f3f958c9bb
ConnorOLeary <themidlvlboss@gmail.com>
2018-02-20 08:30:26 -0700

    Created Linked List

--- DataStructures/Data/CrimeData.cpp
+++ DataStructures/Data/CrimeData.cpp
@@ -8,6 +8,11 @@
 
 #include "CrimeData.hpp"
 
+CrimeData :: CrimeData()
+{
+    //No parameter Constructor required
+}
+
 CrimeData :: CrimeData(string currentCSVLine)
 {
     stringstream parseCSV(currentCSVLine);
